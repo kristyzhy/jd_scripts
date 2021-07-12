aaf2092dfaceeee0d9dff7e63aa94b9fdcbefbe9
kristyzhy <kristyzhy0409@qq.com>
2021-07-12 21:43:07 +0800

    Update jd_fruit.js

--- jd_fruit.js
+++ jd_fruit.js
@@ -91,6 +91,12 @@ async function jdFruit() {
       // option['media-url'] = $.farmInfo.farmUserPro.goodsImage;
       message = `【水果名称】${$.farmInfo.farmUserPro.name}\n`;
       console.log(`\n【京东账号${$.index}（${$.UserName}）的${$.name}好友互助码】${$.farmInfo.farmUserPro.shareCode}\n`);
+      const submitCodeRes = await submitCode();
+      if (submitCodeRes && submitCodeRes.code === 200) {
+         console.log(`🐔东东农场-互助码提交成功！🐔`);
+      }else if (submitCodeRes.code === 300) {
+         console.log(`🐔东东农场-互助码已提交！🐔`);
+      }
       console.log(`\n【已成功兑换水果】${$.farmInfo.farmUserPro.winTimes}次\n`);
       message += `【已兑换水果】${$.farmInfo.farmUserPro.winTimes}次\n`;
       await masterHelpShare();//助力好友
@@ -1360,6 +1366,28 @@ function TotalBean() {
         resolve();
       }
     })
+  })
+}
+function submitCode() {
+    return new Promise(async resolve => {
+    $.get({url: `http://www.helpu.cf/jdcodes/submit.php?code=${$.farmInfo.farmUserPro.shareCode}&type=farm`, timeout: 10000}, (err, resp, data) => {
+      try {
+        if (err) {
+          console.log(`${JSON.stringify(err)}`)
+          console.log(`${$.name} API请求失败，请检查网路重试`)
+        } else {
+          if (data) {
+            data = JSON.parse(data);
+          }
+        }
+      } catch (e) {
+        $.logErr(e, resp)
+      } finally {
+        resolve(data);
+      }
+    })
+    await $.wait(15000);
+    resolve()
   })
 }
 function request(function_id, body = {}, timeout = 1000){
